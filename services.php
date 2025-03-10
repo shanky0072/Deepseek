a3f3e794c6702936a1e7221a333a8f941a1f0a7f
shanky0072 <123097736+shanky0072@users.noreply.github.com>
2025-03-10 18:58:53 +0530

    Create services.php

---
+++ services.php
@@ -0,0 +1,12 @@
+<?php include 'includes/header.php'; ?>
+    <main>
+        <h1>Our Services</h1>
+        <ul>
+            <li>Process Improvement & Lean Consulting</li>
+            <li>Automation & AI-driven Solutions</li>
+            <li>SharePoint & Data Management Optimization</li>
+            <li>Credit Balance & Revenue Cycle Management</li>
+            <li>Training & Change Management</li>
+        </ul>
+    </main>
+<?php include 'includes/footer.php'; ?>
