b070de29033fb20810a238cfed5f009ceee43b52
shanky0072 <123097736+shanky0072@users.noreply.github.com>
2025-03-10 18:58:23 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include 'includes/header.php'; ?>
+    <main>
+        <h1>Welcome to Our Consultancy Firm</h1>
+        <p>We optimize business processes, enhance automation, and integrate AI-driven solutions to improve operational performance.</p>
+    </main>
+<?php include 'includes/footer.php'; ?>
